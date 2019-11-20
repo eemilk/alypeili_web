aab1647bdac4d0cd6202d813fd2dd304c2c1688b
Eemil <eemil.karvonen@gmail.com>
2019-11-20 12:43:21 +0200

    attempting to work around openweather api

---
+++ app.js
@@ -0,0 +1,69 @@
+const tempElement = document.querySelector(".temperature-value p");
+const locationElement = document.querySelector(".location p");
+const iconElement = document.querySelector(".weather-icon");
+const descElement = document.querySelector(".temperature-description p");
+
+// APP CONSTANTS AND VARS
+const KELVIN = 273;
+//API KEY
+const key = "8827c126650cab89acad3db4b1e8917c";
+
+const weather = {};
+
+weather.temperature = {
+    unit : "celcius"
+}
+
+//kelvin to celcius
+weather.temperature.value = 300 - 273;
+
+//check if browser supports geolocation
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    console.log("Error. Cant access location.");
+}
+
+// SET USERS POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+//SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    console.log("Error. Cant access location.");
+}
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
